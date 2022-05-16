e625bbedd40afa44cf3f22f79933a08e12bea50c
ItoxX <laprea.italo@gmail.com>
2022-05-15 23:28:55 -0400

    Update

--- app.js
+++ app.js
@@ -1,56 +1,9 @@
-//Require
-//const fs = require('fs');
 
-//const fs = require('express');
-//const fs = require('./fs');
+const { crearArchivo } = require('./Helpers/muliplicar')
+const argv = require('./config/yargs')
 
-/*let data = "";
+console.clear();
 
-for (let i = 1; i <= 10; i++) {
-
-    data += `${base} * ${i} = ${ base * i } \n`;
-
-}
-
-fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
-
-    if (err) throw err;
-
-    console.log(`El archivo tabla-${base}.txt ha sido creado`);
-
-});*/
-
-const argv = require("./config/yargs").argv;
-
-const { crearArchivo, listarTabla } = require("./multiplicar/multiplicar");
-
-var colors = require('colors');
-
-let comando = argv._[0];
-
-switch (comando) {
-    case "listar":
-        listarTabla(argv.base, argv.limite)
-            .catch(e => console.log(e));
-        break;
-    case "crear":
-        //Se crea el archivo
-        crearArchivo(argv.base, argv.limite)
-            .then(archivo => console.log(`Archivo creado: ${archivo}`))
-            .catch(e => console.log(e));
-        break;
-    default:
-        console.log("Comando no reconocido");
-        break;
-}
-
-
-
-
-
-// let argv2 = process.argv;
-// console.log(argv)
-// console.log("limite", argv.limite);
-/*console.log(argv2);*/
-// let parametro = argv[2];
-// let base = parametro.split("=")[1];
+crearArchivo( argv.b,argv.l,argv.h )
+    .then( nombreArchivo => console.log( nombreArchivo,' creado' ))
+    .catch(err => console.log(err));
